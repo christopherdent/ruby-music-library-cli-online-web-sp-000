37a073660666e026d32c7fdbf12cadde1b37d0df
Christopher Dent <christopherdent01@gmail.com>
2019-09-29 15:30:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,15 +3,23 @@ require_relative '../lib/concerns/findable.rb'
 
 
 class Song 
-
+extend Concerns::Findable
   attr_accessor :name
+  attr_reader :artist, :genre 
+  
    
   
   @@all = []
  
-  def initialize(name)
+  def initialize(name, artist = nil, genre = nil)
     @name = name 
-    save
+    if artist != nil 
+      self.artist = artist 
+    end 
+    if genre != nil
+      self.genre = genre 
+    end   
+    #save 
   end 
   
   def self.all 
@@ -19,51 +27,30 @@ class Song
   end 
   
   def self.destroy_all
-    @@all = []
+    all.clear
   end 
   
   def save
-    @@all << self 
+    self.class.all << self
   end 
   
-  def self.create(name)
-   self.new(name)
+  def self.create(name, artist=nil, genre=nil) 
+   song = new(name)  
+   song.save 
+   song
   end 
   
-  
-  
- def artist=(artist)
+  def artist=(artist)
    @artist = artist
    artist.add_song(self)
-    
- end 
+  end 
  
- 
- 
- def genre=(genre)
-    @genre = genre 
-    genre.songs << self unless genre.songs.include?(self)
- end 
-  
-  
-  def self.find_by_name(name)
-        
-     @@all.detect do |song| 
-       song.name == name 
-     end 
+  def genre=(genre)
+   @genre = genre 
+   genre.songs << self unless genre.songs.include?(self)
   end 
   
-  def self.create_by_name(song_name)
-    self.create(song_name)
-        
- end 
-
-
- 
-  def self.find_or_create_by_name(name)
- 
-    self.find_by_name(name) || self.create_by_name(name)  
-  end 
+  
 
   def self.new_from_filename(filename)
     parts = (filename.gsub(".mp3","").split(" - "))
@@ -73,11 +60,12 @@ class Song
     song.save
     song
   end
-  
+   
   
   def self.create_from_filename(filename)
-    
-    song = Song.new(filename)
+     
+    new_from_filename(filename).save
+
      
   end 
   
