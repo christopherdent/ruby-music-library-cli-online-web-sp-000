e11e3a5ce159cb7f9cc8414bdd4a9b708807536d
Christopher Dent <christopherdent01@gmail.com>
2019-09-29 15:34:01 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,25 +1,6 @@
-class MusicImporter
- 
+class MusicLibraryController
   
-  @@files = []
-  def initialize(path)
-    @path = path 
-    @files = files 
-  end 
   
-  def path 
-    @path 
-  end 
   
-  def files
-   Dir.glob("#{path}/*").map do |file| file.gsub("#{path}/", '') 
-    end
-  end
-
-  def import 
-    files.each do |file|
-      Song.create_from_filename(file)
-    end 
-  end 
-
+  
 end 
